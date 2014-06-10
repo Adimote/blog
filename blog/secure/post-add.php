bdbfdec595f2e09527bcb1c6519ef86a5e30ad31
Adimote <andy@thescrapheap.org>
2014-06-10 21:07:03 +0100

    Removed some tabs

--- blog/secure/post-add.php
+++ blog/secure/post-add.php
@@ -1,56 +1,56 @@
 <?php
-	// TODO: uploading images and returning the location?
-	include_once '../../includes/sql-manager.php';
-	include_once '../common.php';
-	include_once 'post.php';
+// TODO: uploading images and returning the location?
+include_once '../../includes/sql-manager.php';
+include_once '../common.php';
+include_once 'post.php';
 
-	//Main class, handles HTTP Posts of form
-	class PostAddHandler {
-		public $message;
-		private $sqlset;
+//Main class, handles HTTP Posts of form
+class PostAddHandler {
+	public $message;
+	private $sqlset;
 
-		//Constructor
-		public function __construct() {
-			$this->message = new Message();
-			$this->sqlset = new sqlSetter();
-			$this->edit = false;
-		}
+	//Constructor
+	public function __construct() {
+		$this->message = new Message();
+		$this->sqlset = new sqlSetter();
+		$this->edit = false;
+	}
 
-		//Parse a $_POST of the add-a-post form
-		public function parsePost($post){
-			if (post\checkForm($this,$post)) {
-				//$p is either a postID or an sqlError
-				$p = $this->sqlset->addPost(
-					$post['who'],
-					$post['title'],
-					$post['flavour'],
-					$post['content'],
-					$post['date']);
-				if ($p instanceof sqlError) {
-					$this->message = new Message("error",$p->message);
-					#Keep our contents if error
-					$this->post = $post;
-					return;
-				} else {
-					$this->message = new Message("success","Post is posted! ID is $p <a href='post-edit.php?id=$p'>Edit it</a>");
-				}
-				$postID = $p;
-				//Set the tags if it's editing or posting
-				post\setTags($this,$this->sqlset,$post['tags'],$postID);
-			} else {
+	//Parse a $_POST of the add-a-post form
+	public function parsePost($post){
+		if (post\checkForm($this,$post)) {
+			//$p is either a postID or an sqlError
+			$p = $this->sqlset->addPost(
+				$post['who'],
+				$post['title'],
+				$post['flavour'],
+				$post['content'],
+				$post['date']);
+			if ($p instanceof sqlError) {
+				$this->message = new Message("error",$p->message);
 				#Keep our contents if error
 				$this->post = $post;
+				return;
+			} else {
+				$this->message = new Message("success","Post is posted! ID is $p <a href='post-edit.php?id=$p'>Edit it</a>");
 			}
+			$postID = $p;
+			//Set the tags if it's editing or posting
+			post\setTags($this,$this->sqlset,$post['tags'],$postID);
+		} else {
+			#Keep our contents if error
+			$this->post = $post;
 		}
 	}
-	$phr = new PostAddHandler();
+}
+$phr = new PostAddHandler();
 
-	if ($_POST) {
-		//if the user is submitting a post or an edit, apply the changes
-		$phr->parsePost($_POST);
-	}
+if ($_POST) {
+	//if the user is submitting a post or an edit, apply the changes
+	$phr->parsePost($_POST);
+}
 
-	//Place the form
-	post\placeForm($phr);
+//Place the form
+post\placeForm($phr);
 	
 ?>
--- blog/secure/post.php
+++ blog/secure/post.php
@@ -1,130 +1,124 @@
 <?php
-	namespace post;
-	include_once '../common.php';
+namespace post;
+include_once '../common.php';
 
 
-	//decodes a csv string of tags and adds them if they aren't already linked
-	function setTags($phr,$sqlmanager, $tagsString, $postID) {
-		$tags = explode(",",$tagsString);
-		//Clear all the tags
-		$e = $sqlmanager->removeAllTagsFrom($postID);
-		if ($e instanceof sqlError) {
-			$phr->message = new Message("error",htmlspecialchars($e->message));
-			return;
-		}
+//decodes a csv string of tags and adds them if they aren't already linked
+function setTags($phr,$sqlmanager, $tagsString, $postID) {
+	$tags = explode(",",$tagsString);
+	//Clear all the tags
+	$e = $sqlmanager->removeAllTagsFrom($postID);
+	if ($e instanceof sqlError) {
+		$phr->message = new Message("error",htmlspecialchars($e->message));
+		return;
+	}
 
-		//No need to trim the spaces, they're trimmed in attachTag.
-		foreach ($tags as $t) {
-			if (trim($t) != ""){
-				$e = $sqlmanager->attachTag($postID,$t);
-				if ($e instanceof sqlError) {
-					$phr->message = new Message("error",htmlspecialchars($e->message));
-				}
+	//No need to trim the spaces, they're trimmed in attachTag.
+	foreach ($tags as $t) {
+		if (trim($t) != ""){
+			$e = $sqlmanager->attachTag($postID,$t);
+			if ($e instanceof sqlError) {
+				$phr->message = new Message("error",htmlspecialchars($e->message));
 			}
 		}
 	}
+}
 
-	//Check the HTTP POST for any issues
-	function checkForm($phr,$post) {
-	  	if(!array_key_exists('who', $post)) {
-			$phr->message = new \Message("error","HTTP POST FAILED: No Author submitted");
-		} elseif(!array_key_exists('title', $post)) {
-			$phr->message = new \Message("error","HTTP POST FAILED: No Title submitted");
-		} elseif(!array_key_exists('flavour', $post)) {
-			$phr->message = new \Message("error","HTTP POST FAILED: No Flavour Text submitted");
-		} elseif(!array_key_exists('content', $post)) {
-			$phr->message = new \Message("error","HTTP POST FAILED: No Content HTML submitted");
-		} elseif(!array_key_exists('date', $post)) {
-			$phr->message = new \Message("error","HTTP POST FAILED: No Date submitted");
-		//if editid is something other than numbers, throw an error
-		} elseif(preg_match('/[^0-9]/',$post['editid'])) {
-			$phr->message = new \Message("error","HTTP POST FAILED: invalid Edit ID");
-		//if the url name contains1 only numbers, fail because it'll confuse the pagination
-		} elseif(!preg_match('/[^0-9]/',$post['urlname'])) {
-			$phr->message = new \Message("error","HTTP POST FAILED: url for title must contain something other than numbers");
-		} else {
-			return true;
-		}
-		return false;
+//Check the HTTP POST for any issues
+function checkForm($phr,$post) {
+  	if(!array_key_exists('who', $post)) {
+		$phr->message = new \Message("error","HTTP POST FAILED: No Author submitted");
+	} elseif(!array_key_exists('title', $post)) {
+		$phr->message = new \Message("error","HTTP POST FAILED: No Title submitted");
+	} elseif(!array_key_exists('flavour', $post)) {
+		$phr->message = new \Message("error","HTTP POST FAILED: No Flavour Text submitted");
+	} elseif(!array_key_exists('content', $post)) {
+		$phr->message = new \Message("error","HTTP POST FAILED: No Content HTML submitted");
+	} elseif(!array_key_exists('date', $post)) {
+		$phr->message = new \Message("error","HTTP POST FAILED: No Date submitted");
+	//if editid is something other than numbers, throw an error
+	} elseif(preg_match('/[^0-9]/',$post['editid'])) {
+		$phr->message = new \Message("error","HTTP POST FAILED: invalid Edit ID");
+	//if the url name contains only numbers, fail because it'll confuse the pagination
+	} elseif(!preg_match('/[^\-0-9]/',$post['title'])) {
+		$phr->message = new \Message("error","HTTP POST FAILED: url for title must contain something other than numbers");
+	} else {
+		return true;
 	}
+	return false;
+}
 
-	function placeForm($phr) {
-		//Disable Caching, so everything is always re-evaluated
-		header("Cache-Control: no-cache, must-revalidate");
-		$title = \Conf::Title." | ";
-		
-		if ($phr->post) {
-			//Date form element
-			$date = htmlspecialchars($phr->post['date'],ENT_QUOTES);
-			//Author form element
-			$author = htmlspecialchars($phr->post['who'],ENT_QUOTES);
-			//Title form element
-			$postTitle = htmlspecialchars($phr->post['title'],ENT_QUOTES);
-			//urlname
-			$urlname = htmlspecialchars($phr->post['urlname'],ENT_QUOTES);
-			//Flavour Text form element
-			$flavour = htmlspecialchars($phr->post['flavour']);
-			//Content form element
-			$content = json_encode($phr->post['content']);
-			//Tags form element
-			$tags = htmlspecialchars($phr->post['tags']);
-			if ($phr->edit){
-				//Text to go on the submit button
-				$submitName = "Update";
-				//Title of page
-				$title = $title."Edit a Post";
-				//Header of page
-				$head = "Edit a Post on ".\SQLSETTINGS::HOST;
-				//ID for the editid hidden form element
-				$id = $phr->postID;
-				//Optional delete button
-				$delete = <<<"HTML"
+function placeForm($phr) {
+	//Disable Caching, so everything is always re-evaluated
+	header("Cache-Control: no-cache, must-revalidate");
+	$title = \Conf::Title." | ";
+	
+	if ($phr->post) {
+		//Date form element
+		$date = htmlspecialchars($phr->post['date'],ENT_QUOTES);
+		//Author form element
+		$author = htmlspecialchars($phr->post['who'],ENT_QUOTES);
+		//Title form element
+		$postTitle = htmlspecialchars($phr->post['title'],ENT_QUOTES);
+		//Flavour Text form element
+		$flavour = htmlspecialchars($phr->post['flavour']);
+		//Content form element
+		$content = json_encode($phr->post['content']);
+		//Tags form element
+		$tags = htmlspecialchars($phr->post['tags']);
+		if ($phr->edit){
+			//Text to go on the submit button
+			$submitName = "Update";
+			//Title of page
+			$title = $title."Edit a Post";
+			//Header of page
+			$head = "Edit a Post on ".\SQLSETTINGS::HOST;
+			//ID for the editid hidden form element
+			$id = $phr->postID;
+			//Optional delete button
+			$delete = <<<"HTML"
 <a class="btn btn-danger" href="post-remove.php?id={$id}">Delete</a>
 HTML;
-			} else {
-				//Text to go on the submit button
-				$submitName = "Add";
-				//Title of page
-				$title = $title."Add a Post";
-				//Header of page
-				$head = "Add a Post to ".\SQLSETTINGS::HOST;
-				//ID for the editid hidden form element
-				$id = "";
-				//Optional delete button
-				$delete = "";
-			}
 		} else {
+			//Text to go on the submit button
+			$submitName = "Add";
 			//Title of page
 			$title = $title."Add a Post";
 			//Header of page
 			$head = "Add a Post to ".\SQLSETTINGS::HOST;
-			//Date form element
-			$date = date('Y-m-d H:i:s');
-			//Author form element
-			$author = "Author";
-			//Title form element
-			$postTitle = "Title";
-			//Flavour Text form element
-			$flavour = "Flavour Text (usually first 3 lines)";
-			//Content form element
-			$content = "'Content'";
-			//Tags form element
-			$tags = "General, ";
-			//Text to go on the submit button
-			$submitName = "Add";
 			//ID for the editid hidden form element
 			$id = "";
 			//Optional delete button
 			$delete = "";
 		}
+	} else {
+		//Title of page
+		$title = $title."Add a Post";
+		//Header of page
+		$head = "Add a Post to ".\SQLSETTINGS::HOST;
+		//Date form element
+		$date = date('Y-m-d H:i:s');
+		//Author form element
+		$author = "Author";
+		//Title form element
+		$postTitle = "Title";
+		//Flavour Text form element
+		$flavour = "Flavour Text (usually first 3 lines)";
+		//Content form element
+		$content = "'Content'";
+		//Tags form element
+		$tags = "General, ";
+		//Text to go on the submit button
+		$submitName = "Add";
+		//ID for the editid hidden form element
+		$id = "";
+		//Optional delete button
+		$delete = "";
+	}
 
-		$message = $phr->message->formatMessage();
-		echo <<<"HTML"
-<!DOCTYPE html>
-<meta charset='utf-8'>
-<link rel="stylesheet" type="text/css" href="/css/bootstrap.min.css"></link>
-<link rel="stylesheet" type="text/css" href="/css/styles.css"></link>
-<title>{$title}</title>
+	$message = $phr->message->formatMessage();
+	include_once "../view/header.php";
+	echo <<<"HTML"
 <!-- Body -->
 <div class="well">
 <form class="form-horizontal" id="post-form" method='POST'>
@@ -137,15 +131,15 @@ HTML;
 
 		<!-- Date input -->
 		<div class="form-group">
-        	<label class="col-md-2 control-label">Date:</label>
-        	<div class="col-md-9">
+	    	<label class="col-md-2 control-label">Date:</label>
+	    	<div class="col-md-9">
 				<input type="text" class="form-control" name='date' value="{$date}"/>
 			</div>
 		</div>
 
 		<!-- Author input -->
 		<div class="form-group">
-        	<label class="col-md-2 control-label">Author:</label>
+	    	<label class="col-md-2 control-label">Author:</label>
 			<div class="col-md-9">
 				<input type="text" class="form-control" name='who' value="{$author}"/>
 			</div>
@@ -153,11 +147,11 @@ HTML;
 
 		<!-- Title input -->
 		<div class="form-group">
-        	<label class="col-md-2 control-label">Title:</label>
+	    	<label class="col-md-2 control-label">Title:</label>
 			<div class="col-md-9">
 				<div class="input-group">
 					<input type="text" class="form-control" id="title" name='title' value="{$postTitle}"/>
-  					<span class="input-group-addon">url: <span id="urlname">{$urlname}</span></span>
+						<span class="input-group-addon">url: <span id="urlname">{$urlname}</span></span>
 				</div>
 			</div>
 		</div>
@@ -172,9 +166,9 @@ HTML;
 
 		<!-- Content input -->
 		<div class="form-group">
-        	<label class="col-lg-1 control-label">Content:</label>
-        	<div class="col-md-5" >
-        		<div class="rounded-edge">
+	    	<label class="col-lg-1 control-label">Content:</label>
+	    	<div class="col-md-5" >
+	    		<div class="rounded-edge">
 					<div id='content' style='height:500px'></div>
 				</div>
 			</div>
@@ -215,58 +209,61 @@ HTML;
 </div>
 <script src='/js/ace/ace.js'></script>
 <script>
-	function i(id) {return document.getElementById(id);}
-	function preview() {
-		i('html-preview').innerHTML = content.getValue();
+function i(id) {return document.getElementById(id);}
+function preview() {
+	var htmlPreview = i('html-preview');
+	htmlPreview.innerHTML = content.getValue();
+	//update mathJax
+	MathJax.Hub.Queue(["Typeset",MathJax.Hub,htmlPreview]);
+}
+
+var Previewing = true;
+function togglePreview() {
+	if (i('preview-toggle').checked) {
+		Previewing = true;
+		preview();
+		i('preview-button').setAttribute('disabled','disabled');
+	} else {
+		Previewing = false;
+		i('preview-button').removeAttribute('disabled');
 	}
+}
 
-	var Previewing = true;
-	function togglePreview() {
-		if (i('preview-toggle').checked) {
-			Previewing = true;
-			preview();
-			i('preview-button').setAttribute('disabled','disabled');
-		} else {
-			Previewing = false;
-			i('preview-button').removeAttribute('disabled');
-		}
+//ACE Editor Initialisation
+var content = ace.edit('content');
+content.setTheme('ace/theme/monokai');
+content.getSession().setMode('ace/mode/html');
+content.setValue({$content});
+preview();
+//re-preview on any change if it is toggled on
+content.getSession().on('change', function() {
+	if (Previewing) {
+		preview();
 	}
+});
 
-	//ACE Editor Initialisation
-	var content = ace.edit('content');
-	content.setTheme('ace/theme/monokai');
-	content.getSession().setMode('ace/mode/html');
-	content.setValue({$content});
-	preview();
-	//re-preview on any change if it is toggled on
-	content.getSession().on('change', function() {
-		if (Previewing) {
-			preview();
-		}
-	});
-
-	window.onload = function() {
-		var title = i("title");
-		var update = function(){
-			var text = title.value;
-			text = text.toLowerCase().trim();
-			text = text.replace(/[\t\b ]+/g, "-");
-			text = text.replace(/[^a-z0-9\-]/g, "");
-			i("urlname").innerHTML = escape(text);
-		};
-		title.onkeydown = update;
-		title.onchange = update;
-		title.onpaste = update;
-		title.oninput = update;
-		//call it on start
-		update();
-
-		//When the content is submitted, copy all of the text in the main box to a hidden input
-		i("post-form").onsubmit = function() {
-			i("content-inserter").value = content.getValue();
-		};
+window.onload = function() {
+	var title = i("title");
+	var update = function(){
+		var text = title.value;
+		text = text.toLowerCase().trim();
+		text = text.replace(/[\t\b ]+/g, "-");
+		text = text.replace(/[^a-z0-9\-]/g, "");
+		i("urlname").innerHTML = escape(text);
 	};
+	title.onkeydown = update;
+	title.onchange = update;
+	title.onpaste = update;
+	title.oninput = update;
+	//call it on start
+	update();
+
+	//When the content is submitted, copy all of the text in the main box to a hidden input
+	i("post-form").onsubmit = function() {
+		i("content-inserter").value = content.getValue();
+	};
+};
 </script>
 HTML;
-	}
+}
 ?>
